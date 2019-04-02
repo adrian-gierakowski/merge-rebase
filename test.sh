04b9a99e7e1bae531d63d208c1aa1d0ade004eb8
Adrian Gierakowski <agierakowski@gmail.com>
2019-04-02 10:25:44 +0100

    initial commit

---
+++ test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+test_file () {
+  local content=$(<$1)
+  printf "test for $1: "
+  if [[ "$content" != "$1" ]]; then
+    echo "FAILED! ${content} is not equal ${1}"
+    exit 1
+  else
+    echo "PASSED!"
+  fi
+
+}
+
+test_copy () {
+  local content1=$(<$1)
+  printf "test for $1 and $2: "
+  local content2=$(<$2)
+  if [[ "$content1" != "$content2" ]]; then
+    echo "FAILED! ${content1} is not equal ${content2}"
+    exit 1
+  else
+    echo "PASSED!"
+  fi
+}
+
+test_file file1
+test_file file3
+test_file file2
+test_file file4
+
+test_copy file1 file1_copy
